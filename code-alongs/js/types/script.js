673e010e64ab4b14b872c1ffad40814199b9015f
Sunpreet Kaur <sunpreet@192-168-1-102.tpgi.com.au>
2023-06-05 14:33:38 +1000

    adds operators content

---
+++ code-alongs/js/types/script.js
@@ -0,0 +1,37 @@
+//console.log
+
+console.log("It's linked correctly");
+
+//
+console.log(typeof 15); // Prediction: {your answer here} number
+console.log(typeof 5.5); // Prediction: ...      number
+console.log(typeof NaN); // Prediction:          number
+console.log(typeof 'hello'); // Prediction:      string
+console.log(typeof true); // Prediction:         boolean
+console.log(typeof (1 != 2)); // Prediction:      boolean
+console.log(typeof ('hamburger' + 's')); // Prediction:   string
+console.log(typeof ('hamburgers' - 's')); // Prediction: NaN number
+console.log(typeof ('1' + '3')); // Prediction:   string
+console.log(typeof ('1' - '3')); // Prediction:    -2  number
+console.log(typeof ('johnny' + 5)); // Prediction: string
+console.log(typeof ('johnny' - 5)); // Prediction: NaN number
+console.log(typeof (99 * 'luftbaloons')); // Prediction: Nan  number
+
+//
+
+// let number1 =prompt("First Number");
+// console.log(number1);
+
+// let number2 = prompt("Second Number");
+// console.log(number2);
+
+// let answer;
+// answer = parseInt(number1) + parseInt(number2);
+// console.log(answer);
+
+
+
+let firstName = "Sunpreet";
+let lastName = "Kaur";
+let age = 28;
+console.log("Hi, my name is "+firstName+" "+lastName+" and I am "+age+" years old.")
